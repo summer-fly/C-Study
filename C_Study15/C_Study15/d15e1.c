ad8d810c4a9c5c620a702875aa47e62fe309b13b
summer-fly <2361968268@qq.com>
2021-03-07 20:55:01 +0800

    第十五天代码

---
+++ C_Study15/C_Study15/d15e1.c
@@ -0,0 +1,61 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+
+
+
+
+
+
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//ÊýÖµ½»»»
+//void swap2(int *x, int *y)
+//{
+//	int tmp = *x;
+//	*x = *y;
+//	*y = tmp;
+//}
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	swap2(&a, &b);
+//	printf("a = %d b = %d", a, b);
+//	return 0;
+//}
+
+//void swap1(int x, int y)
+//{
+//	int tmp = x;
+//	x = y;
+//	y = tmp;
+//	return x, y;
+//	
+//}
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	swap1(a,b);
+//	printf("a = %d b = %d", a, b);
+//	return 0;
+//}
