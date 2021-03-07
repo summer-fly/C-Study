b630e2224b3ee95536569cb5815435060a0b45ee
summer-fly <2361968268@qq.com>
2021-03-07 20:50:27 +0800

    第十四天代码

---
+++ C_Study14/C_Study14/d14e1.c
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+
+//º¯Êý
+int add(int x, int y)
+{
+	int z;
+	z = x + y;
+}
+
+
+int main()
+{
+	int a = 10;
+	int b = 20;
+	int num;
+	num = add(a, b);
+	printf("%d", num);
+
+
+
+
+
+
+
+	return 0;
+}
