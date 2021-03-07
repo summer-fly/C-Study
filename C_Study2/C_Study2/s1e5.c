086ad9f4123cc1c67ad474b2047ba5d8faf5a123
summer-fly <2361968268@qq.com>
2021-03-07 20:22:59 +0800

    第二天代码

---
+++ C_Study2/C_Study2/s1e5.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+// 完成while循环语句
+// 完成一个函数
+
+int ADD(int x, int y)
+{
+	int z = x + y;
+	return z;
+}
+
+int main()
+{
+	int a = 10;
+	int b = 23;
+	int sum;
+	sum = ADD(a, b);
+	printf("a与b的和是：%d\n", sum);
+
+
+
+
+
+
+
+
+
+
+
+	/*int Line = 0;
+	printf("开始学习C语言！");
+	
+	while (Line < 20000)
+	{
+		printf("打一行代码:%d\n",Line);
+		Line++;
+
+	}
+	printf("学习完成！");*/
+
+
+
+	return 0;
+}
