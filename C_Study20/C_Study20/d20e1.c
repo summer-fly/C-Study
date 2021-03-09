37ce3607e1c5d4b7c89eb877eebde711b521a3a9
summer-fly <2361968268@qq.com>
2021-03-09 22:51:35 +0800

    第二十天代码

---
+++ C_Study20/C_Study20/d20e1.c
@@ -0,0 +1,92 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+
+//求n的阶乘
+//int Fac1(int n)
+//{
+//	int i = 0;
+//	int ret = 1;
+//	for (i = 1; i <= n; i++)
+//	{
+//		ret *= i;
+//	}
+//	return ret;
+//}
+//
+//int Fac2(int n)
+//{
+//	if (n <= 1)
+//	{
+//		return 1;
+//	}
+//	else
+//	{
+//		return n * Fac2(n - 1);
+//	}
+//}
+//int main()
+//{
+//	int n;
+//	scanf("%d", &n);
+//	int ret;
+//	ret = Fac2(n);
+//	printf("%d", ret);
+//	return 0;
+//}
+
+//斐波那契数列
+//int Fib(int n)
+//{
+//	if (n <= 2)
+//	{
+//		return 1;
+//	}
+//	else
+//	{
+//		return Fib(n - 1) + Fib(n - 2);
+//	}
+//}
+//int main()
+//{
+//	int n = 0;
+//	int ret = 0;
+//	scanf("%d",&n);
+//	ret = Fib(n);
+//	printf("%d",ret);
+//	return 0;
+//}
+
+//int Fib(int n)
+//{
+//	int a = 1;
+//	int b = 1;
+//	int c = 1;
+//	while (n > 2)
+//	{
+//		c = a + b;
+//		a = b;
+//		b = c;
+//		n--;
+//	}
+//	return c;
+//}
+//int main()
+//{
+//	int n = 0;
+//	int ret = 0;
+//	scanf("%d", &n);
+//	ret = Fib(n);
+//	printf("%d", ret);
+//	
+//	
+//	return 0;
+//}
+
+int main()
+{
+	int arr[10];   //数组的创建
+	char arr1[5];
+
+	return 0;
+}
