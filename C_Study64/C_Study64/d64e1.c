1e347bc19e20287b55bab1b2782556a40c100006
Summer168 <2361968268@qq.com>
2021-06-23 21:20:11 +0800

    第六十四天代码

---
+++ C_Study64/C_Study64/d64e1.c
@@ -0,0 +1,54 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+#include<assert.h>
+
+
+
+//int main()
+//{
+//	
+//	//feof(); //EOF -- end of file -文件结束标志
+//	FILE* pf = fopen("test.txt", "r");
+//	if (pf == NULL)
+//	{
+//		return 0;
+//	}
+//	int ch = fgetc(pf);
+//	printf("%d\n", ch);
+//
+//	fclose(pf);
+//	pf = NULL;
+//
+//
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	//printf("%s\n", strerror(errno));
+//	FILE* pf = fopen("test2.txt", "r");
+//	if (pf = NULL)
+//	{
+//		perror("hhh");
+//		return 0;
+//	}
+//
+//	fclose(pf);
+//	pf = NULL;
+//
+//	return 0;
+//}
+
+
+
+int main()
+{
+
+
+
+
+	return 0;
+}
